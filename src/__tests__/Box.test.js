1e68a526d984fa5218f6587bd2ff9bd3ba29e32a
Innocent Kateba <katebasoft@gmail.com>
2019-05-15 14:48:08 -0500

    Adds Test for Box Structure

--- src/__tests__/Box.test.js
+++ src/__tests__/Box.test.js
@@ -3,13 +3,25 @@ import App from '../App';
 import Box from '../Box.js';
 import {shallow} from 'enzyme';
 
+const props = {
+  value: "",
+  onClick: ()=>{}
+}
+
+const BoxTest = (
+  <button onClick={props.onClick}>{props.value}</button>
+)
+
 
 describe('Box Tests', ()=>{
-//Checks if button is present on Box Component
     it('Box rendering', ()=>{
-      const box = shallow(<Box/>);
-      expect(box.contains(<button/>)).toEqual(true);
+      const box = shallow(<Box {...props} />);
+      //Checks if button is present on Box Component
+      //expect(box.contains(<button/>)).toEqual(true);
+      //Check the whole structure of Box Component
+      expect(box.equals(BoxTest)).toEqual(true);
     });
   
+    
   
   });
